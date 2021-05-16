3c19e53a937500fab367b46f1463f3affe739299
Pedro Santos <pedro.hssop@gmail.com>
2021-05-16 18:19:37 -0300

    Serviço de Encriptação
    Inicialmente usado para gerar o hash das senhas dos usuários.

---
+++ LocaLabs/LocaLabs.Application/Services/EncryptService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.ObjectPool;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace LocaLabs.Application.Services
+{
+    internal interface IEncryptService
+    {
+        string GetHashFrom(string value);
+    }
+
+    internal class EncryptService : IEncryptService
+    {
+        ObjectPool<StringBuilder> BuilderPool { get; }
+
+        public EncryptService(ObjectPool<StringBuilder> builderPool) =>
+            BuilderPool = builderPool;
+
+        public string GetHashFrom(string value)
+        {
+            using var md5Hash = MD5.Create();
+            var data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(value));
+
+            var sBuilder = BuilderPool.Get();
+            for (int i = 0; i < data.Length; i++)
+                sBuilder.Append(data[i].ToString("x2"));
+
+            var output = sBuilder.ToString();
+            BuilderPool.Return(sBuilder);
+
+            return output;
+        }
+    }
+}
