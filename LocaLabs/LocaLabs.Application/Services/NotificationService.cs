34aac48e29863db9199450fc832e74264b80a7d7
Pedro Santos <pedro.hssop@gmail.com>
2021-05-16 18:18:56 -0300

    Serviço de Notificação
    Este serviço concentra as notificações do escopo corrente e permite acesso centralizado à elas.
    O controle do escopo e através do tipo de injeção na API.

---
+++ LocaLabs/LocaLabs.Application/Services/NotificationService.cs
@@ -0,0 +1,58 @@
+﻿using LocaLabs.Domain.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LocaLabs.Application.Services
+{
+    public interface INotificationService
+    {
+        bool HasErrors { get; }
+        IEnumerable<string> GetNotifications();
+        
+        void AddError(Exception ex);
+        void AddNotification(params string[] messages);
+        void AddNotification(params EntityValidation[] validations);
+    }
+
+    internal class NotificationService : INotificationService
+    {
+        public bool HasErrors { get; set; }
+        private HashSet<string> Messages { get; set; }
+
+        public void AddError(Exception ex)
+        {
+            HasErrors = true;
+            AddNotification($"[ERROR] - {ex.Message}");
+        }
+
+        public void AddNotification(params string[] messages)
+        {
+            if (messages.Length == 0)
+                return;
+
+            if (Messages is null)
+                Messages = new HashSet<string>();
+
+            foreach (var item in messages)
+            {
+                if (!string.IsNullOrEmpty(item))
+                    Messages.Add(item);
+            }
+        }
+
+        public void AddNotification(params EntityValidation[] validations)
+        {
+            if (validations.Length > 0)
+                AddNotification(validations.Where(w => w is not null).Select(s => s.ToString()).ToArray());
+        }
+
+        public IEnumerable<string> GetNotifications()
+        {
+            if (Messages is null)
+                return Enumerable.Empty<string>();
+
+            return Messages;
+        }
+    }
+}
