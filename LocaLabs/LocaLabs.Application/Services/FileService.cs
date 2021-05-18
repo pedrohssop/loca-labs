0d962b321ed83759cff3581b296b4109d586849a
Pedro Santos <pedro.hssop@gmail.com>
2021-05-18 11:37:36 -0300

    FileService
    Serviço para criação de arquivos.

---
+++ LocaLabs/LocaLabs.Application/Services/FileService.cs
@@ -0,0 +1,35 @@
+﻿using DinkToPdf;
+
+namespace LocaLabs.Application.Services
+{
+    public interface IFileService
+    {
+        byte[] AsPdf(string content);
+    }
+
+    public class FileService : IFileService
+    {
+        public byte[] AsPdf(string htmlContent)
+        {
+            var converter = new SynchronizedConverter(new PdfTools());
+            var doc = new HtmlToPdfDocument()
+            {
+                GlobalSettings = {
+                    ColorMode = ColorMode.Color,
+                    PaperSize = PaperKind.A4Plus,
+                    Orientation = Orientation.Landscape,
+                },
+                Objects = {
+                    new ObjectSettings() {
+                        PagesCount = true,
+                        HtmlContent = htmlContent,
+                        WebSettings = { DefaultEncoding = "utf-8" },
+                        HeaderSettings = { FontSize = 9, Right = "Page [page] of [toPage]", Line = true, Spacing = 2.812 }
+                    }
+                }
+            };
+
+            return converter.Convert(doc);
+        }
+    }
+}
